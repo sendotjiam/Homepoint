ffa12e1631419f66bafdff1b837ee22e6e8e3876
Sendo Tjiamis <sendotjiamis9898@gmail.com>
2022-05-25 19:32:03 +0700

    init

---
+++ homepoint/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  homepoint
+//
+//  Created by Sendo Tjiam on 25/05/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window : UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = ViewController()
+        window?.rootViewController = UINavigationController(rootViewController: vc)
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ homepoint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homepoint
+//
+//  Created by Sendo Tjiam on 25/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
