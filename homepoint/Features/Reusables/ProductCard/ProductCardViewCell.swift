32175029d0498c8b9fb24c67d8684ea9cdc56b61
Sendo Tjiamis <sendotjiamis9898@gmail.com>
2022-05-31 10:30:56 +0700

    Add product card view cell (not yet fixed)

---
+++ homepoint/Features/Reusables/ProductCard/ProductCardViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCardViewCell.swift
+//  homepoint
+//
+//  Created by Sendo Tjiam on 31/05/22.
+//
+
+import UIKit
+
+class ProductCardViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
