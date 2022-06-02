3e5cf6ff847ee450db71594c67f8e322cff076fa
Sendo Tjiamis <sendotjiamis9898@gmail.com>
2022-06-02 20:00:49 +0700

    remove conflicts

---
+++ homepoint/Features/Reusables/ProductCard/LargeProductCardViewCell.swift
@@ -0,0 +1,50 @@
+//
+//  LargeProductCardViewCell.swift
+//  homepoint
+//
+//  Created by Sendo Tjiam on 31/05/22.
+//
+
+import UIKit
+
+class LargeProductCardViewCell: UICollectionViewCell {
+
+    // MARK: - Outlets
+    @IBOutlet weak var containerView : UIView!
+    @IBOutlet weak var imageView : UIImageView!
+    @IBOutlet weak var nameLabel : UILabel!
+    @IBOutlet weak var priceLabel : UILabel!
+    @IBOutlet weak var ratingLabel : UILabel!
+    @IBOutlet weak var soldNumberLabel : UILabel!
+    
+    // MARK: - Data
+    var data : [String: Any] = [:] {
+        didSet { configureCell() }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupUI()
+    }
+
+}
+
+extension LargeProductCardViewCell {
+    private func setupUI() {
+        containerView.addBorder(
+            width: 1,
+            color: .black
+        )
+        containerView.roundedCorner(with: 8)
+        containerView.dropShadow(
+            with: 0.05,
+            radius: 2,
+            offset: CGSize(width: 0, height: 1)
+        )
+    }
+    
+    private func configureCell() {
+        
+    }
+}
+
