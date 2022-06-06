06f36fcbfd75df9680bcd1e8eedbeaa14fb7bdd3
Sendo Tjiamis <sendotjiamis9898@gmail.com>
2022-06-06 20:38:14 +0700

    Fix tabbar with dummy viewcontroller

--- homepoint/Features/Reusables/TabBar/CustomTabBarController.swift
+++ homepoint/Features/Reusables/TabBar/CustomTabBarController.swift
@@ -12,116 +12,112 @@ class CustomTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tabBar.isTranslucent = false
-        tabBar.backgroundColor = .white
-        
-        if #available(iOS 13.0, *) {
-            let tabBarAppearance: UITabBarAppearance = UITabBarAppearance()
-            tabBarAppearance.configureWithDefaultBackground()
-            tabBarAppearance.backgroundColor = UIColor.white
-            UITabBar.appearance().standardAppearance = tabBarAppearance
-            
-            if #available(iOS 15.0, *) {
-                UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
-            }
-        }
-        
-        self.delegate = self
-        
-        let ex1 = ExViewController1()
-        let ex2 = ExViewController2()
-        let vc = ViewController()
-        let ex3 = ExViewController2()
-        let ex4 = ExViewController2()
+        let ex1 = A()
+        let ex2 = B()
+        let vc = C()
+        let ex3 = D()
+        let ex4 = E()
         
         let navEx1 = UINavigationController(rootViewController: ex1)
         let navEx2 = UINavigationController(rootViewController: ex2)
         let navVC = UINavigationController(rootViewController: vc)
         let navEx3 = UINavigationController(rootViewController: ex3)
         let navEx4 = UINavigationController(rootViewController: ex4)
-        
-        var idx = 0
+    
         let titles = [
             "Beranda",
-            "Beranda",
-            nil,
+            "Wishlist",
+            "Scan",
+            "Pesanan",
             "Profil",
-            "Beranda",
         ]
-        
         let images = [
-            UIImage(named: "ic_profile"),
-            UIImage(named: "ic_profile"),
-            nil,
-            UIImage(named: "ic_profile"),
+            UIImage(named: "ic_home.selected"),
+            UIImage(named: "ic_heart"),
+            UIImage(named: "ic_scan"),
+            UIImage(named: "ic_notes"),
             UIImage(named: "ic_profile")
         ]
-        [navEx1, navEx3, navVC, navEx4, navEx2].forEach { nav in
-            if idx == 2 {
-                nav.tabBarItem = UITabBarItem(
-                    title: nil,
-                    image: nil,
-                    selectedImage: nil
-                )
-                nav.tabBarItem.isEnabled = false
-            } else {
-                nav.tabBarItem = UITabBarItem(
-                    title: titles[idx],
-                    image: images[idx],
-                    selectedImage: images[idx]
-                )
-            }
+        let selectedImages = [
+            UIImage(named: "ic_home.selected"),
+            UIImage(named: "ic_heart.selected"),
+            UIImage(named: "ic_scan.selected"),
+            UIImage(named: "ic_notes.selected"),
+            UIImage(named: "ic_profile.selected")
+        ]
+        setupTabBar(
+            [navEx1, navEx3, navVC, navEx4, navEx2],
+            titles,
+            images,
+            selectedImages
+        )
+    }
+}
+
+extension CustomTabBarController {
+    private func setupTabBar(
+        _ navControllers : [UINavigationController],
+        _ titles: [String],
+        _ images: [UIImage?],
+        _ selectedImages : [UIImage?]
+    ) {
+        tabBar.isTranslucent = false
+        tabBar.backgroundColor = .white
+        tabBar.tintColor = .red
+        tabBar.barTintColor = .black
+        tabBar.layer.shadowColor = UIColor.black.cgColor
+        tabBar.layer.shadowOpacity = 0.2
+        tabBar.layer.shadowRadius = 4
+        tabBar.layer.shadowOffset = CGSize(width: 0, height: 1)
+        var idx = 0
+        navControllers.forEach { nav in
+            nav.tabBarItem = UITabBarItem(
+                title: titles[idx],
+                image: images[idx],
+                selectedImage: selectedImages[idx]
+            )
             idx += 1
         }
         setViewControllers(
-            [navEx1, navEx2, navVC, navEx3, navEx4],
+            navControllers,
             animated: false
         )
-        
-        self.hidesBottomBarWhenPushed = true
-    
-        let tabBar = self.tabBar as! CustomTabBar
-//        tabBar.didTapButton = {
-//            print("TESTET")
-//        }
+        self.selectedIndex = 2
+        hidesBottomBarWhenPushed = true
     }
-    
-//    func setupMiddleBtn() {
-//        let btnSize : CGFloat = 70
-//        let btn = UIButton(
-//            frame: CGRect(
-//                x: (self.view.bounds.width / 2) - (btnSize / 2),
-//                y: -30,
-//                width: btnSize,
-//                height: btnSize
-//            )
-//        )
-//        btn.layer.masksToBounds = true
-//        btn.setBackgroundImage(UIImage(named: "ic_barcode"), for: .normal)
-//        btn.addTarget(self, action: #selector(scanBtnTapped), for: .touchUpInside)
-//        btn.layer.zPosition = 1
-//        self.view.layoutIfNeeded()
-//        self.tabBar.addSubview(btn)
-//    }
-//
-//    @objc func scanBtnTapped(sender: UIButton) {
-//        print("TesT")
-//        self.present(ViewController(), animated: true)
-//    }
-    
-//    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
-//        if self.isHidden {
-//            return super.hitTest(point, with: event)
-//        }
-//
-//        let from = point
-//        let to = middleButton.center
-//
-//        return sqrt((from.x - to.x) * (from.x - to.x) + (from.y - to.y) * (from.y - to.y)) <= 39 ? middleButton : super.hitTest(point, with: event)
-//    }
 }
 
-// MARK: - UITabBarController Delegate
-extension CustomTabBarController: UITabBarControllerDelegate {
+class A : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
 
+class B : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
+class C : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
+class D : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
+class E : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
 }
