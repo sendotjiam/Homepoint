a7f4fff038fc5ade7f73973c0bf33435b196b533
Sendo Tjiamis <sendotjiamis9898@gmail.com>
2022-07-02 18:47:22 +0700

    Moved long whatsapp url string to Cosntants file

--- homepoint/Helpers/Constants/Constants.swift
+++ homepoint/Helpers/Constants/Constants.swift
@@ -10,6 +10,11 @@ import Foundation
 struct Constants {
     static let BaseUrl = "https://homepoint-server-staging.herokuapp.com/"
     
+    // MARK: - Helpers
+    static let UrlWhatsappApp = "whatsapp://send?phone=6282114742197&text=Halo%20Homepoint%F0%9F%99%8C%F0%9F%8F%BB%0AAda%20yang%20ingin%20Saya%20tanyakan%2C%20nih!%0A%0A(Tuliskan%20pertanyaanmu%20disini%20ya!)"
+    static let UrlWhatsappWeb = "https://api.whatsapp.com/send?phone=6282114742197&text=Halo%20Homepoint%F0%9F%99%8C%F0%9F%8F%BB%0AAda%20yang%20ingin%20Saya%20tanyakan%2C%20nih!%0A%0A(Tuliskan%20pertanyaanmu%20disini%20ya!)"
+    
+    
     // MARK: - CustomTabBar
     static let Home = "Beranda"
     static let HomeIcon = "ic_home"
