3de1a7a1da0533b9aaf67105da054aed7188dfba
Sendo Tjiamis <sendotjiamis9898@gmail.com>
2022-07-22 18:14:46 +0700

    Add upload media in confirmation payment VC

--- homepoint/Features/Payment/ViewControllers/ConfirmPayment/ConfirmPaymentViewController.swift
+++ homepoint/Features/Payment/ViewControllers/ConfirmPayment/ConfirmPaymentViewController.swift
@@ -20,6 +20,12 @@ final class ConfirmPaymentViewController: UIViewController {
     @IBOutlet weak var reuploadButton: UIButton!
     @IBOutlet weak var uploadButton: UIButton!
     @IBOutlet weak var confirmButton: UIButton!
+    @IBOutlet weak var mediaImageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    // MARK: - Variables
+    private var hasUploaded = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,15 +38,15 @@ final class ConfirmPaymentViewController: UIViewController {
     }
     
     @IBAction func reuploadButtonTapped(_ sender: Any) {
-        print("REUPLOAD")
+        showImagePickerOptions()
     }
     
     @IBAction func uploadButtonTapped(_ sender: Any) {
-        print("UPLOAD")
+        showImagePickerOptions()
     }
     
     @IBAction func confirmButtonTapped(_ sender: Any) {
-        let vc = InformationAlertViewController()
+        let vc = ConfirmationAlertViewController()
         vc.modalPresentationStyle = .overFullScreen
         present(vc, animated: false)
     }
@@ -51,11 +57,69 @@ extension ConfirmPaymentViewController {
         deadlineStackView.roundedCorner(with: 8)
         confirmButton.roundedCorner(with: 8)
         accountInformationView.addBorder(width: 1, color: ColorCollection.blueLigthColor.value)
+        imagePicker.delegate = self
+        confirmButton.isEnabled = false
+    }
+    
+    private func setupAfterUpload() {
+        if !hasUploaded { hasUploaded = true }
+        if hasUploaded {
+            // Disable upload button
+            uploadButton.isHidden = true
+            uploadButton.isEnabled = false
             
-        if uploadButton.image(for: .normal) == nil {
-            reuploadButton.isHidden = true
-//            confirmButton.isEnabled = false
-//            confirmButton.backgroundColor = ColorCollection.grayTextColor.value
+            reuploadButton.isHidden = false
+            confirmButton.isEnabled = true
+            confirmButton.backgroundColor = ColorCollection.yellowColor.value
+        } else {
+            confirmButton.isEnabled = false
+            confirmButton.backgroundColor = ColorCollection.grayTextColor.value
         }
+    }
+    
+    private func showImagePickerOptions() {
+        let alert = UIAlertController(
+            title: "Pilih Foto",
+            message: nil,
+            preferredStyle: .actionSheet
+        )
+        alert.addAction(
+            UIAlertAction(
+                title: "Library",
+                style: .default,
+                handler: { [weak self] _ in
+                    guard let self = self else { return }
+                    self.imagePicker.sourceType = .photoLibrary
+                    self.present(self.imagePicker, animated: true)
+                }
+            )
+        )
+        alert.addAction(
+            UIAlertAction(
+                title: "Cancel",
+                style: .cancel,
+                handler: { [weak self] _ in
+                    self?.dismiss(animated: true)
+                }
+            )
+        )
+        self.present(alert, animated: true)
+    }
+}
+
+extension ConfirmPaymentViewController :
+    UIImagePickerControllerDelegate,
+    UINavigationControllerDelegate {
+    func imagePickerController(
+        _ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
+    ) {
+        if let pickedImage = info[.originalImage] as? UIImage {
+            mediaImageView.contentMode = .scaleAspectFit
+            mediaImageView.image = pickedImage
+            mediaImageView.backgroundColor = .clear
+        }
+        setupAfterUpload()
+        dismiss(animated: true)
     }
 }
